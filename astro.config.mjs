4b94e547fdd4395573194d53c72fda9f08f34350
Data88 <andrew1on1@gmail.com>
2023-04-22 21:40:54 +0200

    ./dist as out dir

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,7 +25,7 @@ const __dirname = dirname(__filename);
 
 export default defineConfig( /** @type {import('astro').AstroUserConfig} */{
   root: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
-  outDir: './',       // When running `astro build`, path to final static output
+  outDir: './dist',       // When running `astro build`, path to final static output
   publicDir: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
   output: 'server',
   site: 'https://53c.co.za/',
