80e7865d09c3cdb4e7265b7d66b85070f2feb47e
Data88 <andrew1on1@gmail.com>
2023-04-22 21:39:48 +0200

    built in apex

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,16 +18,15 @@ const __dirname = dirname(__filename);
 // helpful tooltips, and warnings if your exported object is invalid.
 // You can disable this by removing "@ts-check" and `@type` comments below.
 
-// @ts-check
+@ts-check
 
 
 // https://astro.build/config
 
-// https://astro.build/config
 export default defineConfig( /** @type {import('astro').AstroUserConfig} */{
-  // root: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
-  // outDir: './dist',       // When running `astro build`, path to final static output
-  // publicDir: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
+  root: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
+  outDir: './',       // When running `astro build`, path to final static output
+  publicDir: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
   output: 'server',
   site: 'https://53c.co.za/',
   // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
