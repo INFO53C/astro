5f431317d501ed83f200b753fb10b86b50bdb743
Data88 <andrew1on1@gmail.com>
2023-04-23 05:38:55 +0200

    config no dist

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import svelte from '@astrojs/svelte';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-import mdx from '@astrojs/mdx';
-import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
-import markdoc from "@astrojs/markdoc";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import svelte from '@astrojs/svelte'
+import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
+import mdx from '@astrojs/mdx'
+import { defineConfig } from "astro/config"
+import vercel from "@astrojs/vercel/serverless"
+import markdoc from "@astrojs/markdoc"
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 // Full Astro Configuration API Documentation:
 // https://docs.astro.build/reference/configuration-reference
@@ -18,7 +18,7 @@ const __dirname = dirname(__filename);
 // helpful tooltips, and warnings if your exported object is invalid.
 // You can disable this by removing "@ts-check" and `@type` comments below.
 
-@ts-check
+// @ts-check
 
 
 // https://astro.build/config
@@ -26,7 +26,7 @@ const __dirname = dirname(__filename);
 export default defineConfig( /** @type {import('astro').AstroUserConfig} */{
   root: './',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
   // outDir: './dist',       // When running `astro build`, path to final static output
-  publicDir: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
+  // publicDir: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
   output: 'server',
   site: 'https://53c.co.za/',
   // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
